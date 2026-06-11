3bf05c2c6bae7ac9a0135239939e10c72ebcf1a3
SongChaeyoung <call0512@naver.com>
2026-06-11 11:14:20 +0900

    chore: enable browser terminal logging

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: {
+    browserToTerminal: true,
+  },
 };
 
 export default nextConfig;
